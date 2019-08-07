02eaf7df82f3ed4688e551126d6153b2f1023cd1
Raimundo Luiz <rayfcrols@gmail.com>
2019-08-07 18:03:25 -0300

    Step 1

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const routes = require("./routes");
 
 const server = express();
+
+mongoose.connect(
+  "mongodb+srv://omnistack:kill6133@cluster0-utkyw.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
 
 server.use(express.json());
 
