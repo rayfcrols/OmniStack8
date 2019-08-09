56d148f67172f0b292ef76fe89029d68cae45e22
Raimundo Luiz <rayfcrols@gmail.com>
2019-08-09 14:50:56 -0300

    Conclusão do Beackend
    Adicionado teste na rotina de devcontroller

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -2,15 +2,41 @@ const axios = require("axios");
 const Dev = require("../models/Dev");
 
 module.exports = {
+  async index(req, res) {
+    const { user } = req.headers;
+
+    const loggedDev = await Dev.findById(user);
+
+    const users = await Dev.find({
+      $and: [
+        { _id: { $ne: user } },
+        { _id: { $nin: loggedDev.likes } },
+        { _id: { $nin: loggedDev.dislikes } }
+      ]
+    });
+    return res.json(users);
+  },
+
   async store(req, res) {
     const { username } = req.body;
     const userExists = await Dev.findOne({ user: username });
 
     if (userExists) return res.json(userExists);
 
-    const response = await axios.get(
-      `https://api.github.com/users/${username}`
-    );
+    // const response = await axios.get(
+    //   `https://api.github.com/users/${username}`
+    // );
+
+    let response = null;
+    try {
+      response = await axios.get(`https://api.github.com/users/${username}`);
+    } catch (err) {
+      response = err.response;
+      return res.status(400).json({ error: "Dev not exists" });
+    }
+
+    if (response.data.name == null)
+      return res.status(400).json({ error: "Dev not exists" });
 
     const { name, bio, avatar_url: avatar } = response.data;
 
